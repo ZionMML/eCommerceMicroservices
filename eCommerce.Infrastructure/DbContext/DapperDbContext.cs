1ffe887bc69e475a8280957ca2a834b242a82c3f
ZionMML <zion82@gmail.com>
2025-10-31 21:19:43 +0800

    Dapper ExecuteAsync

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace eCommerce.Infrastructure.DbContext
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace eCommerce.Infrastructure.DbContext
 {
-    internal class DapperDbContext
+    public class DapperDbContext
     {
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _connection;
+        public DapperDbContext(IConfiguration confiugration)
+        {
+            _configuration = confiugration;
+            string? connectionString = 
+                _configuration.GetConnectionString("PostgresConnection");
+
+            // Create a new NpgsqlConnecttion with the retrieved connection string
+            _connection = new NpgsqlConnection(connectionString);
+        }
+
+        public IDbConnection DbConnection => _connection;
     }
 }
