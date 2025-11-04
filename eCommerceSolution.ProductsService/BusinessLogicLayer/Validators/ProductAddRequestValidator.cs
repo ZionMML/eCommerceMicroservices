ff366a234499cee6f709503685e38c18d7672fc8
ZionMML <zion82@gmail.com>
2025-11-05 01:00:07 +0800

    Added Products Fluent Validation

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+
+public class ProductAddRequestValidator : AbstractValidator<ProductAddRequest>
+{
+    public ProductAddRequestValidator()
+    {
+        // ProductName
+        RuleFor(x => x.ProductName)
+            .NotEmpty().WithMessage("Product name is required.");
+
+        // Category
+        RuleFor(x => x.Category)
+            .IsInEnum().WithMessage("Category is incorrect.");
+
+        // UnitPrice
+        RuleFor(x => x.UnitPrice)
+            .InclusiveBetween(0, double.MaxValue)
+            .WithMessage($"Unit price must be between 0 to {double.MaxValue}.");
+
+        // QuantityInStock
+        RuleFor(x => x.QuantityInStock)
+            .InclusiveBetween(0, int.MaxValue)
+            .WithMessage($"Quantity in stock must be between 0 to {int.MaxValue}.");
+    }
+}
+
---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        // ProductID
+        RuleFor(x => x.ProductID)
+            .NotEmpty().WithMessage("Product ID is required.");
+
+        // ProductName
+        RuleFor(x => x.ProductName)
+            .NotEmpty().WithMessage("Product name is required.");
+
+        // Category
+        RuleFor(x => x.Category)
+            .IsInEnum().WithMessage("Category is incorrect.");
+
+        // UnitPrice
+        RuleFor(x => x.UnitPrice)
+            .InclusiveBetween(0, double.MaxValue)
+            .WithMessage($"Unit price must be between 0 to {double.MaxValue}.");
+
+        // QuantityInStock
+        RuleFor(x => x.QuantityInStock)
+            .InclusiveBetween(0, int.MaxValue)
+            .WithMessage($"Quantity in stock must be between 0 to {int.MaxValue}.");
+    }
+}
+
