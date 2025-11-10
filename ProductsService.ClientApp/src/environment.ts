742773b95176bfbf175e2cc45d97f3eef0573a31
ZionMML <zion82@gmail.com>
2025-11-10 18:53:39 +0800

    Added Angular client app for ProductsService

---
+++ ProductsService.ClientApp/src/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7264/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'https://localhost:7047/api/products/'
+  };
+  
