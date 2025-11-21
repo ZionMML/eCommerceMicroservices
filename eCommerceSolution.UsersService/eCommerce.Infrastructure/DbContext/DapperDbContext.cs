5b6027408f063ec025895d31a64b43f8d4d6ba53
ZionMML <zion82@gmail.com>
2025-11-21 16:40:40 +0800

    Added Environment Variables into Users Service

--- eCommerceSolution.UsersService/eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -12,8 +12,13 @@ namespace eCommerce.Infrastructure.DbContext
         public DapperDbContext(IConfiguration confiugration)
         {
             _configuration = confiugration;
-            string? connectionString = 
-                _configuration.GetConnectionString("PostgresConnection");
+            string connectionStringTemplate = 
+                _configuration.GetConnectionString("PostgresConnection")!;
+            string connectionString = connectionStringTemplate
+                .Replace("$POSTGRES_HOST",
+                Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+                .Replace("$POSTGRES_PASSWORD",
+                Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
 
             // Create a new NpgsqlConnecttion with the retrieved connection string
             _connection = new NpgsqlConnection(connectionString);
