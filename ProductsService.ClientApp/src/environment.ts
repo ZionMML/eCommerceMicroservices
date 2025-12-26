f1b627df23654b16023e8ac9b2f16b9640d6660c
ZionMML <zion82@gmail.com>
2025-12-26 18:33:08 +0800

    Changed https to http in ProductsService and UsersService environment files

--- ProductsService.ClientApp/src/environment.ts
+++ ProductsService.ClientApp/src/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7264/api/auth/', // API base URL for development,
-    productsMicroserviceUrl: 'https://localhost:7047/api/products/'
+    apiUrl: 'http://localhost:9090/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'http://localhost:5144/api/products/'
   };
   
