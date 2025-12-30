af505cad300e06e47ba04566f7bc5f094e6a01a8
ZionMML <zion82@gmail.com>
2025-12-30 12:13:44 +0800

    Return Dummy Data instead of Exception Error

--- eCommerceSolution.OrdersService/BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
+++ eCommerceSolution.OrdersService/BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
@@ -24,8 +24,13 @@ public class UsersMicroserviceClient(HttpClient httpClient)
            }
            else
            {
-                throw new HttpRequestException($"Http request failed with" +
-                    $"status code {response.StatusCode}");
+                //throw new HttpRequestException($"Http request failed with" +
+                //    $"status code {response.StatusCode}");
+
+                return new UserDTO(PersonName: "Temporarily Unavailable",
+                    Email: "Temporarily Unavailable",
+                    Gender: "Temporarily Unavailable",
+                    UserID: Guid.Empty);
             }
         }
 
