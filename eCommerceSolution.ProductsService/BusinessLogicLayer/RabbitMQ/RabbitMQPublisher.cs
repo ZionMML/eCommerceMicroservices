db321eb18370ebf0021acceffe49ec5966c0876f
ZionMML <zion82@gmail.com>
2026-03-19 15:01:48 +0800

    Created RabbitMQPublisher class for the connection and channel

--- eCommerceSolution.ProductsService/BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
@@ -1,23 +1,43 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
 
 namespace eCommerce.ProductsService.BusinessLogicLayer.RabbitMQ
 {
-    public class RabbitMQPublisher : IRabbitMQPublisher
+    public class RabbitMQPublisher : IRabbitMQPublisher, IDisposable
     {
         private readonly IConfiguration _configuration;
+        private IConnection _connection;
+        private IChannel _channel;
 
         public RabbitMQPublisher(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
 
-            string hostName = _configuration["RabbitMQ_HostName"]!;
-            string userName = _configuration["RabbitMQ_UserName"]!; 
-            string password = _configuration["RabbitMQ_Password"]!;
-            string port = _configuration["RabbitMQ_Port"]!;
+        public async Task InitializeRabbitMQAsync()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQ_HostName"]!,
+                UserName = _configuration["RabbitMQ_UserName"]!,
+                Password = _configuration["RabbitMQ_Password"]!,
+                Port = Convert.ToInt32(_configuration["RabbitMQ_Port"]!)
+            };
+
+            _connection = await factory.CreateConnectionAsync();
+
+            _channel = await _connection.CreateChannelAsync();
         }
         public void Publish<T>(string routingKey, T message)
         {
             throw new NotImplementedException();
         }
+
+        public void Dispose()
+        {
+            _channel.Dispose();
+            _connection.Dispose();
+        }   
     }
 }
