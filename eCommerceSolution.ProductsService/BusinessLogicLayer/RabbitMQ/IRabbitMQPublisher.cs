919ddab3ecfe13dc9e560798a5d266ed71fb710e
ZionMML <zion82@gmail.com>
2026-03-19 11:50:30 +0800

    RabbitMQ Nuget Package and Publisher Interface and Class

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/RabbitMQ/IRabbitMQPublisher.cs
@@ -0,0 +1,8 @@
+﻿
+namespace eCommerce.ProductsService.BusinessLogicLayer.RabbitMQ
+{
+    public interface IRabbitMQPublisher
+    {
+        void Publish<T>(string routingKey, T message);
+    }
+}
